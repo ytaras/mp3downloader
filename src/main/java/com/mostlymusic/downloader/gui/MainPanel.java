3dad516e4a8b16d56048bbdc39a8657247219c8d
ytaras <yura.taras@gmail.com>
2011-09-20 16:55:25 +0300

    Switching to items list on login

--- src/main/java/com/mostlymusic/downloader/gui/MainPanel.java
+++ src/main/java/com/mostlymusic/downloader/gui/MainPanel.java
@@ -2,6 +2,7 @@ package com.mostlymusic.downloader.gui;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.mostlymusic.downloader.dto.Account;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,14 +17,22 @@ public class MainPanel extends JPanel {
     private static final String ACCOUNTS = "ACCOUNTS";
     private static final String ITEMS = "ITEMS";
     private CardLayout cardLayout;
+    private ApplicationModel model;
 
     @Inject
-    public MainPanel(AccountsList accountsList, Items items) {
+    public MainPanel(AccountsList accountsList, Items items, ApplicationModel model) {
         cardLayout = new CardLayout();
         setLayout(cardLayout);
         setAccountsList(accountsList);
         setItems(items);
         cardLayout.show(this, ACCOUNTS);
+        this.model = model;
+        model.addListener(new ApplicationModelListenerAdapter() {
+            @Override
+            public void loggedIn(Account account) {
+                cardLayout.show(MainPanel.this, ITEMS);
+            }
+        });
     }
 
     private void setAccountsList(AccountsList accountsList) {
