1dda60d8315b6ccefdc060ed23333b83427f5d2e
ytaras <yura.taras@gmail.com>
2011-11-04 18:59:17 +0200

    Minimizing to tray

--- src/main/java/com/mostlymusic/downloader/gui/Main.java
+++ src/main/java/com/mostlymusic/downloader/gui/Main.java
@@ -9,6 +9,10 @@ import com.mostlymusic.downloader.localdata.SchemaCreator;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.sql.SQLException;
 
 /**
@@ -38,16 +42,53 @@ public class Main {
             }
         });
         if (SystemTray.isSupported()) {
-            SystemTray systemTray = SystemTray.getSystemTray();
-            Image image = Toolkit.getDefaultToolkit().getImage(Main.class.getResource("/favicon.gif"));
-            TrayIcon trayIcon = new TrayIcon(image);
-            trayIcon.setImageAutoSize(true);
-            try {
-                systemTray.add(trayIcon);
-            } catch (AWTException e) {
-                e.printStackTrace();
-            }
+            addTray(frame);
         }
+    }
+
+    private static void addTray(final JFrame frame) {
+        SystemTray systemTray = SystemTray.getSystemTray();
+        Image image = Toolkit.getDefaultToolkit().getImage(Main.class.getResource("/favicon.gif"));
+        ActionListener showWindowAction = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(true);
+                frame.toFront();
+            }
+        };
+        PopupMenu popupMenu = getTrayMenu(showWindowAction);
+
+        TrayIcon trayIcon = new TrayIcon(image, "MostlyMusic Download Manager", popupMenu);
+
+        trayIcon.setImageAutoSize(true);
+        trayIcon.addActionListener(showWindowAction);
+        try {
+            systemTray.add(trayIcon);
+            frame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowIconified(WindowEvent e) {
+                    frame.setVisible(false);
+                }
+            });
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static PopupMenu getTrayMenu(ActionListener showWindowAction) {
+        PopupMenu popupMenu = new PopupMenu();
+        MenuItem restore = new MenuItem("Restore");
+        restore.addActionListener(showWindowAction);
+        popupMenu.add(restore);
+        MenuItem exit = new MenuItem("Exit");
+        exit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        popupMenu.add(exit);
+        return popupMenu;
     }
 
 }
