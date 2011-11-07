2dd46b32498c15b9bf1a005a8da6b10a7df2302b
ytaras <yura.taras@gmail.com>
2011-11-07 13:39:44 +0200

    Showing login dialog (strange issue with IoC)

--- src/main/java/com/mostlymusic/downloader/gui/JDialogLoginDialog.java
+++ src/main/java/com/mostlymusic/downloader/gui/JDialogLoginDialog.java
@@ -1,8 +1,10 @@
 package com.mostlymusic.downloader.gui;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author ytaras
@@ -11,15 +13,23 @@ import javax.swing.*;
  */
 @Singleton
 public class JDialogLoginDialog implements LoginDialog {
-    public JDialogLoginDialog() {
+    private final ApplicationModel applicationModel;
+
+    @Inject
+    public JDialogLoginDialog(ApplicationModel applicationModel) {
+        this.applicationModel = applicationModel;
     }
 
     @Override
     public void showDialog(MainWindow window) {
         JDialog dialog = new JDialog(window, "Login", true);
-        dialog.getContentPane().add(new JLabel("Hello, world"));
+        AccountsList accountsList = new AccountsList();
+        accountsList.setModel(applicationModel);
+        JPanel contentPane = accountsList.getContentPane();
+        dialog.getContentPane().add(contentPane, BorderLayout.CENTER);
         dialog.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         dialog.pack();
+        dialog.setResizable(true);
         dialog.setLocationRelativeTo(window);
         dialog.setVisible(true);
     }
