18fd1a737efa52b67710059ccb7226dcf67fbf55
ytaras <yura.taras@gmail.com>
2012-04-28 15:44:14 +0300

    Fixed exception on drag

--- src/main/java/com/mostlymusic/downloader/gui/components/MoveMouseListener.java
+++ src/main/java/com/mostlymusic/downloader/gui/components/MoveMouseListener.java
@@ -17,10 +17,14 @@ public class MoveMouseListener implements MouseListener, MouseMotionListener {
 
     Point getScreenLocation(MouseEvent e) {
         Point cursor = e.getPoint();
-        // TODO Fix
-        Point target_location = this.target.getLocationOnScreen();
-        return new Point((int) (target_location.getX() + cursor.getX()),
-                (int) (target_location.getY() + cursor.getY()));
+        try {
+            Point target_location = this.target.getLocationOnScreen();
+            return new Point((int) (target_location.getX() + cursor.getX()),
+                    (int) (target_location.getY() + cursor.getY()));
+        } catch (RuntimeException ex) {
+            ex.printStackTrace(System.err);
+        }
+        return null;
     }
 
     public void mouseClicked(MouseEvent e) {
@@ -42,13 +46,20 @@ public class MoveMouseListener implements MouseListener, MouseMotionListener {
 
     public void mouseDragged(MouseEvent e) {
         Point current = this.getScreenLocation(e);
-        Point offset = new Point((int) current.getX() - (int) start_drag.getX(),
-                (int) current.getY() - (int) start_drag.getY());
-        Window frame = (Window) SwingUtilities.getRoot(target);
-        Point new_location = new Point(
-                (int) (this.start_loc.getX() + offset.getX()), (int) (this.start_loc
-                .getY() + offset.getY()));
-        frame.setLocation(new_location);
+        if (current == null) {
+            return;
+        }
+        try {
+            Point offset = new Point((int) current.getX() - (int) start_drag.getX(),
+                    (int) current.getY() - (int) start_drag.getY());
+            Window frame = (Window) SwingUtilities.getRoot(target);
+            Point new_location = new Point(
+                    (int) (this.start_loc.getX() + offset.getX()), (int) (this.start_loc
+                    .getY() + offset.getY()));
+            frame.setLocation(new_location);
+        } catch (RuntimeException ex) {
+            ex.printStackTrace(System.err);
+        }
     }
 
     public void mouseMoved(MouseEvent e) {
